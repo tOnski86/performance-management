498ec12721652e25727813f6adcf2c1a32c67f19
Antonio Saqueton <code.antoniohicetasaqueton@gmail.com>
2024-12-19 06:50:16 +0800

    Remove header border

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,6 @@ import Heading from './Heading';
 
 const StyledHeader = styled.header`
   padding: 3rem 4.8rem;
-  border-bottom: 0.1rem solid var(--color-grey-light-2);
 
   @media (min-width: 1024px) {
     padding: 2rem 4.8rem;
